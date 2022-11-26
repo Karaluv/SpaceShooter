ce8b83e601cbec9368b785e421204ab1d6f1edcb
Chikalkin Sergey <chikalkin.sd@phystech.edu>
2022-11-26 20:12:13 +0300

    Continue to work with class Object_Management

--- src/game_core.cpp
+++ src/game_core.cpp
@@ -14,6 +14,33 @@ int main()
 	_CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF);
 	srs::start_render();
 	{
+
+		//for simple test
+		double basic_coord = 0;
+		double basic_speed = 1;
+		double basic_accel = 0;
+		double dt = 0.01;
+		// test meanings of starting parametres
+
+		Object_Management Manager;
+		Manager.create_object(3, 1); //creation of spaceship
+		double*** data = new double**[3];
+		for (unsigned k = 0; k < 3; ++ k)
+		{
+			data[k] = new double* [1000];
+			for (unsigned j = 0; j < 1000; ++j) {
+				data[k][j] = new double[3];
+			}
+		}
+		for (unsigned i = 0; i < 3; ++i) {
+			data[0][0][i] = basic_coord;
+		}
+		data[1][0][0] = basic_speed;
+		data[1][0][1] = 0;
+		data[1][0][2] = 0;
+
+		//end of the code for the simple test
+
 		// create monkey and cube and several lights in different positions and angels
 		srs::create_object("monkey", 0, 0, -1, 0, 0, 0, 1);
 		srs::create_object("cube", 4, 0, -1, 0, 0, 0, 1);
@@ -25,6 +52,7 @@ int main()
 		// write some code 144 times per seconds to update camera position, objects position, lights position and color
 		for (int i = 0; i < 10000; i++)
 		{
+			Manager.update_object(data, 1);
 			// rotate monkey by sin i around y axis
 			srs::update_object(0, 0, 0, -1, 0, sin(float(i) / 100), 0, 1);
 			// move cube by sin i around y axis
