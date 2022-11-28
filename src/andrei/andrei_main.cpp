ab6b87e2ba5351632bbc8958d3fc4929a20e27bd
infa2021Vasilev <vasilev.andrei@phystech.edu>
2022-11-28 20:19:33 +0300

    Finaly, some good rotating

--- src/andrei/andrei_main.cpp
+++ src/andrei/andrei_main.cpp
@@ -1,9 +1,9 @@
 #include <iostream>
 #include <cassert>
 #include <matrix.cpp>
-#include <valarray>
 
-
+// super accurate sin, tan, cos functions
+#include <cmath>
 
 
 // template T class body with fields m, tensor of inertia as matrix 3x3 , rx, ry, rz, w
@@ -17,15 +17,19 @@ public:
 	directed_segment<T> angle;
 	directed_segment<T> w;
 	T size;
+	T kinetic_energy_of_rotation;
 
 	Body(T m, matrix<T, 3> tensor, directed_segment<T> r, directed_segment<T> v, directed_segment<T> angle, directed_segment<T> w, T size) :
-		m(m), tensor(tensor), r(r), v(v), angle(angle), w(w), size(size) {};
-	Body() : m(0), tensor(), r(), v(), angle(), w(), size(0) {};
+		m(m), tensor(tensor), r(r), v(v), angle(angle), w(w), size(size) {
+		kinetic_energy_of_rotation = (w * (tensor * w)) / 2;
+	};
+	Body() : m(0), tensor(), r(), v(), angle(), w(), size(0) {
+		kinetic_energy_of_rotation = 0;
+	};
 	
 	//rule of five
-	
-	Body(const Body& other) : m(other.m), tensor(other.tensor), r(other.r), v(other.v), angle(other.angle), w(other.w), size(other.size) {};
-	Body(Body&& other) : m(other.m), tensor(other.tensor), r(other.r), v(other.v), angle(other.angle), w(other.w), size(other.size) {};
+	Body(const Body& other) : m(other.m), tensor(other.tensor), r(other.r), v(other.v), angle(other.angle), w(other.w), size(other.size), kinetic_energy_of_rotation(other.kinetic_energy_of_rotation) {};
+	Body(Body&& other) : m(other.m), tensor(other.tensor), r(other.r), v(other.v), angle(other.angle), w(other.w), size(other.size), kinetic_energy_of_rotation(other.kinetic_energy_of_rotation) {};
 	Body& operator=(const Body& other) {
 		m = other.m;
 		tensor = other.tensor;
@@ -34,8 +38,9 @@ public:
 		angle = other.angle;
 		w = other.w;
 		size = other.size;
+		kinetic_energy_of_rotation = other.kinetic_energy_of_rotation;
 		return *this;
-	}
+	};
 	Body& operator=(Body&& other) {
 		m = other.m;
 		tensor = other.tensor;
@@ -44,9 +49,11 @@ public:
 		angle = other.angle;
 		w = other.w;
 		size = other.size;
+		kinetic_energy_of_rotation = other.kinetic_energy_of_rotation;
 		return *this;
-	}
-	~Body() = default;
+	};
+	~Body() {};
+	
 
 
 	
@@ -57,17 +64,28 @@ public:
 	}
 
 	void update_w(T time) {
-		w[0] = w[0] + time * w[1] * w[2] * (tensor[1][1] - tensor[2][2]) / tensor[0][0];
-		w[1] = w[1] + time * w[0] * w[2] * (tensor[2][2] - tensor[0][0]) / tensor[1][1];
-		w[2] = w[2] + time * w[0] * w[1] * (tensor[0][0] - tensor[1][1]) / tensor[2][2];
+		directed_segment<T> w_;
+		w_[0] = w[1] * w[2] * (tensor[1][1] - tensor[2][2]) / tensor[0][0];
+		w_[1] = w[0] * w[2] * (tensor[2][2] - tensor[0][0]) / tensor[1][1];
+		w_[2] = w[0] * w[1] * (tensor[0][0] - tensor[1][1]) / tensor[2][2];
+
+
+		
+		w[0] = w[0] + time * w_[0];
+		w[1] = w[1] + time * w_[1];
+		w[2] = w[2] + time * w_[2];
+
+		T kin = (w * (tensor * w)) / 2;
+		T k = sqrt(kinetic_energy_of_rotation/kin);
+		w = w * k;
 	}	
 	
 	void update_angle(T time) {
 		directed_segment<T> angle_;
-		angle_[0] = (w[0] / cos(angle[2]) + w[1] / sin(angle[2])) / (sin(angle[1]) * (tan(angle[2]) + 1/tan(angle[2])));
-		angle_[1] = (w[0] / sin(angle[2]) - w[1] / cos(angle[2])) / (tan(angle[2]) + 1/tan(angle[2]));
+
+		angle_[0] = (w[0] / cos(angle[2]) + w[1] / sin(angle[2])) * sin(2 * angle[2]) / (2 * sin(angle[1]));
+		angle_[1] = (w[0] / sin(angle[2]) - w[1] / cos(angle[2])) * sin(2*angle[2]) / (2);
 		angle_[2] = w[2] - angle_[0]*cos(angle[1]);
-		
 		angle = angle + angle_ * time;
 	}
 
@@ -83,7 +101,10 @@ public:
 		other.v = v2;
 	}
 
-	//calculation of kinetic moment using tensor of inertia and angular velocity
+	//get kinetic energy including rotation using tensor
+	T get_kinetic_energy() {
+		return (m*(v * v))/2+(w * (tensor * w))/2;
+	}
 	
 	
 	
